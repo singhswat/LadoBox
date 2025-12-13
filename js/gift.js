f6e5fcb32b482f432f2f95c77ec798ab8db9eaa0
Swatantra <proxybon@gmail.com>
2025-12-13 13:46:04 +0000

    index fexied

---
+++ js/gift.js
@@ -0,0 +1,8 @@
+{
+    id: "GFT123",
+        type: "shares",
+            stock: "Reliance",
+                quantity: 2,
+                    status: "Pending",
+                        createdAt: "2025-01-10"
+}
